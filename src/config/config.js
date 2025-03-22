0663b2151d2862aab6a605428edaeb20b17f2144
Jonad Dhrami <jdreamer@192.168.1.126>
2025-03-22 13:45:18 +0000

    fix ingress yml

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,8 @@
 // src/config.js
 
-const MINIKUBE_IP = "http://192.168.49.2:30000"
-
 const config = {
-  API_URL: MINIKUBE_IP + "/api", // You can change this URL as per your backend location
+  API_URL: "http://backend.local/api", // Update to match Ingress
 };
 
 export default config;
+
